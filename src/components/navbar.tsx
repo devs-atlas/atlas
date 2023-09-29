f72cdc7aef23c021ae09c2a7dd01f0400e9caece
Barrett Ruth <br.barrettruth@gmail.com>
2023-09-29 14:17:34 -0400

    feat: first posts

---
+++ src/components/navbar.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import styles from './Navbar.module.css'
+
+export default function Navbar() {
+  return (
+    <ul className={styles.navbar}>
+      <li className={styles.navItem}>
+        <div className={styles.icon}>
+          <Image
+            priority
+            src="/icon.svg"
+            alt="atlas icon"
+            width={20}
+            height={20}
+          />
+          <Link href="/">atlas</Link>
+        </div>
+      </li>
+      <li className={styles.navItem}>
+        <Link href="/about">about</Link>
+      </li>
+    </ul>
+  )
+}
