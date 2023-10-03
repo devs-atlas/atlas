e12fe25def74340b5fffc538c38e9e83d6d53bc0
Barrett Ruth <br.barrettruth@gmail.com>
2023-10-03 19:01:28 -0400

    feat: about

---
+++ src/pages/about.tsx
@@ -0,0 +1,57 @@
+import Image from 'next/image'
+import Layout from '~/components/layout'
+
+import Link from 'next/link'
+import { Separator } from '~/components/separator'
+import { garamond } from '~/styles/fonts'
+import styles from './About.module.css'
+
+export default function About() {
+  return (
+    <Layout>
+      <div className={`${styles.aboutContainer} ${garamond.className}`}>
+        <div className={styles.profile}>
+          <Image
+            src="/nic.webp"
+            alt="Nicholas Hoffs profile"
+            width={200}
+            height={200}
+          />
+          <div className={`${styles.profileTextContainer}`}>
+            <div className={`${styles.name}`}>Nicholas Hoffs</div>
+            <div className={`${styles.profileText}`}>
+              Nicholas is a second year studying Computer Science at the
+              University of Virginia. He specializes in transformers and time
+              series forecasting and is the brains behind atlas.
+            </div>
+          </div>
+        </div>
+        <Separator numCircles={7} width={'60%'} />
+        <div className={styles.profile}>
+          <Image
+            src="/barrett.webp"
+            alt="Barrett Ruth profile"
+            width={200}
+            height={200}
+          />
+          <div className={`${styles.profileTextContainer}`}>
+            <div className={`${styles.name}`}>Barrett Ruth</div>
+            <div className={`${styles.profileText}`}>
+              Barrett is a second year studying Computer Science and Economics
+              at the University of Virginia. He is passionate about writing
+              efficient, clean software and the open source community. View his
+              website{' '}
+              <Link
+                style={{ textDecoration: 'underline' }}
+                href="https://barrett-ruth.github.io"
+              >
+                here
+              </Link>
+              .
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
+}
