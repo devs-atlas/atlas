2c24009aae551edad40d19419675af9c42511b4c
Barrett Ruth <br.barrettruth@gmail.com>
2023-10-03 14:50:29 -0400

    fix: use consistent fonts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,13 +6,13 @@ import { Fragment } from 'react'
 import { Separator } from '~/components/separator'
 import type { PostMeta } from '~/lib/posts'
 import { getAllPostMeta } from '~/lib/posts'
-import { manrope } from '~/styles/fonts'
+import { fragment } from '~/styles/fonts'
 import styles from './Meta.module.css'
 
 function Meta({ id, title, description, date, image }: PostMeta) {
   return (
     <Link href={`/posts/${id}`}>
-      <div className={`${styles.meta} ${manrope.className}`}>
+      <div className={`${styles.meta} ${fragment.className}`}>
         <div className={styles.metaContainer}>
           <h2 className={styles.metaTitle}>{title}</h2>
           <Date className={styles.metaDate} dateString={date} />
--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,8 +1,6 @@
-import { EB_Garamond, Fragment_Mono, Manrope, Raleway } from 'next/font/google'
+import { EB_Garamond, Fragment_Mono } from 'next/font/google'
 
 const fragment = Fragment_Mono({ subsets: ['latin'], weight: '400' })
 const garamond = EB_Garamond({ subsets: ['latin'] })
-const raleway = Raleway({ subsets: ['latin'] })
-const manrope = Manrope({ subsets: ['latin'] })
 
-export { fragment, garamond, manrope, raleway }
+export { fragment, garamond }
