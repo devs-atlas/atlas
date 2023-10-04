3c0adfaa829cbad64074269ad38584161abbddbc
Barrett Ruth <br.barrettruth@gmail.com>
2023-10-03 20:51:02 -0400

    navbar: try to prefetch header

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -20,7 +20,9 @@ export default function Navbar() {
         </div>
       </li>
       <li className={styles.navItem}>
-        <Link href="/about">about</Link>
+        <Link href="/about" prefetch>
+          about
+        </Link>
       </li>
     </ul>
   )
